4c3c121fb7833d60d98195ad4c9e64fafbe23058
Willian Borges <134416210+WBorges1984@users.noreply.github.com>
2024-09-05 23:20:06 -0300

    Finalizado a tela de resultados

--- src/pages/resultado/resultado.jsx
+++ src/pages/resultado/resultado.jsx
@@ -1,59 +1,86 @@
-import React from 'react';
-import { View, Text, Image, FlatList } from 'react-native';
-import styles from './resultado.style.js';
+import React, { useState } from "react";
+import { View, Text, Image, FlatList } from "react-native";
+import styles from "./resultado.style.js";
 import logo from "../../assets/cart.png";
+import ButtonBottom from "../../components/ButtonBottom/ButtonBottom.jsx";
 
-export default function Resultado() {
+export default function Resultado({navigation}) {
   const data = [
     {
       id: "1", // Adicione uma chave única para cada item
       dt: "2024-09-01",
       time: "20",
       acertos: "21",
-      percent: "70"
+      percent: "70",
+    },
+     {
+       id: "2", // Adicione uma chave única para cada item
+       dt: "2024-09-02",
+       time: "28",
+       acertos: "18",
+       percent: "60",
+     },
+    {
+      id: "3", // Adicione uma chave única para cada item
+      dt: "2024-09-02",
+      time: "29",
+      acertos: "25",
+      percent: "83",
     },
     {
-        id: "2", // Adicione uma chave única para cada item
-        dt: "2024-09-02",
-        time: "28",
-        acertos: "18",
-        percent: "60"
+      id: "4", // Adicione uma chave única para cada item
+      dt: "2024-09-02",
+      time: "27",
+      acertos: "16",
+      percent: "30",
     },
     {
-        id: "3", // Adicione uma chave única para cada item
-        dt: "2024-09-02",
-        time: "29",
-        acertos: "25",
-        percent: "83"
+      id: "5", // Adicione uma chave única para cada item
+      dt: "2024-09-02",
+      time: "29",
+      acertos: "16",
+      percent: "30",
     },
     {
-        id: "4", // Adicione uma chave única para cada item
-        dt: "2024-09-02",
-        time: "27:20",
-        acertos: "16",
-        percent: "30"
+      id: "6", // Adicione uma chave única para cada item
+      dt: "2024-09-02",
+      time: "27",
+      acertos: "16",
+      percent: "30",
     },
   ];
 
   // Componente para renderizar cada item da lista
   const renderItem = ({ item }) => (
     <View style={styles.item}>
-      <Text>Data: {item.dt}</Text>
-      <Text>Hora: {item.time}</Text>
-      <Text>Acertos: {item.acertos}</Text>
-      <Text>Percentual: {item.percent}%</Text>
+      <View style={styles.ItemId}>
+        <ButtonBottom texto={item.id} fullW/>
+      </View>
+      <View style={parseInt(item.percent) > 69 ? styles.dadosItemGreen : styles.dadosItemRed}>
+        <View>
+          <Text style={styles.textoDT}>Data: {item.dt}</Text>
+          <Text style={styles.textoDT}>Tempo: {item.time}min</Text>
+        </View>
+        <View style={styles.dadosPerc}>
+          <Text style={styles.textoAcertos}>{item.acertos}/30</Text>
+          <Text style={styles.textoPorcento}>{item.percent}%</Text>
+        </View>
+      </View>
     </View>
   );
 
   return (
     <View style={styles.container}>
       <Image source={logo} style={styles.logo} />
-      <Text style={styles.texto}>Resultado</Text>
-      <FlatList
+      <Text style={styles.texto}>Resultado1</Text>
+      <FlatList style={styles.flat}
         data={data}
         renderItem={renderItem}
-        keyExtractor={item => item.id} // Use a chave única correta
+        keyExtractor={(item) => item.id} // Use a chave única correta
       />
+      <View style={styles.btn}>
+        <ButtonBottom colorBackBlue texto="Menu principal" fullW onPress={()=>navigation.navigate("InicialLogado")}/>
+      </View>
     </View>
   );
 }
--- src/pages/resultado/resultado.style.js
+++ src/pages/resultado/resultado.style.js
@@ -1,5 +1,6 @@
 import { StyleSheet } from "react-native";
 import { COLORS, FONT_SIZE } from "../../constants/theme";
+import { Colors } from "react-native/Libraries/NewAppScreen";
 
 
 const styles = StyleSheet.create({
@@ -7,15 +8,81 @@ const styles = StyleSheet.create({
         flex: 1,
         backgroundColor: COLORS.white,
         alignItems: "center",
-        paddingTop: 40
+        paddingTop: 40,
+        padding: 20,
     },
     texto:{
         fontSize: FONT_SIZE[32],
         color:COLORS.blue
     },
     item:{
-        backgroundColor:COLORS.grayLight,
+        flexDirection:"row",
+        width:"100%",
+       
+    },
+    ItemId:{
+        backgroundColor: COLORS.blue,
+        width: "auto",
+        paddingBottom: 10,
+        paddingTop: 10,
+        paddingLeft: 20,
+        paddingRight: 20,
+        borderRadius: 5,
+        marginBottom: 20,
         
+    },
+    textoDT:{
+        fontSize:FONT_SIZE[16],
+        color: COLORS.white
+    },
+    textoAcertos:{
+        fontSize:FONT_SIZE[16],
+        color: COLORS.white,
+        textAlignVertical:"bottom"
+    },
+    textoPorcento:{
+        fontSize:FONT_SIZE[48],
+        color: COLORS.white,
+        fontWeight: "bold",
+        textAlignVertical:"bottom",
+        marginLeft:10
+    },
+    dadosItemGreen:{
+        width:"100%",
+        paddingLeft: 10,
+        paddingRight: 10,
+        paddingTop:10,
+        paddingBottom: 10,
+        marginBottom: 20,
+        backgroundColor:COLORS.greenLight,
+        flexDirection:"row",
+        borderRadius: 10,
+        gap: 20,
+        
+    },
+    dadosItemRed:{
+        width:"100%",
+        paddingLeft: 10,
+        paddingRight: 10,
+        paddingTop:10,
+        paddingBottom: 10,
+        marginBottom: 20,
+        backgroundColor:COLORS.red,
+        flexDirection:"row",
+        borderRadius: 10,
+        gap: 20,
+        
+    },
+    dadosPerc:{
+        flexDirection: "row",
+        marginRight: 20
+    },
+    btn:{
+        width:"100%",
+    },
+    flat:{
+        marginBottom: 10,
+        marginTop: 10
     }
 });
 
